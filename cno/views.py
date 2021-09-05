fc353113b0209cbcd3714a083923f5dcaba2c6cb
Tejas Mandre <tejasmandre0306@gmail.com>
2021-09-05 18:23:42 +0530

    lots of refactoring

--- cno/views.py
+++ cno/views.py
@@ -22,14 +22,9 @@ class HomeViewSet(ViewSet):
         data = request.POST
         lvl_id = data.get("lvl_id", None)
 
-        labels = [i['education_level'] for i in Education.objects.values('education_level').distinct()]
-
-        edu_dict = {course: 0 for course in labels}
-
         access_levels = {}
 
-        for lvl in range(1, 11):
-
+        for lvl in range(1, 10):
             access_level = Education.objects.filter(**{"lvl{}_id".format(lvl): lvl_id}).first()
             if access_level:
                 break
@@ -39,27 +34,46 @@ class HomeViewSet(ViewSet):
         self.__class__.global_qs = queryset
 
         grad_dict = self.grad_dict_generator(queryset)
-
         profession_dict = self.profession_dict_generator(queryset)
+        edu_dict = self.edu_dict_generator(queryset)
+        access_levels = self.access_levels_generator(queryset)
 
-        user_count_per_education_level = queryset.values('education_level').annotate(distinct=Count("user_id"))
+        return render(request, "home.html", {"access_levels": access_levels, "educationLevel": edu_dict, "gradDict": grad_dict, "professionDict": profession_dict})
 
-        for _ in user_count_per_education_level:
+    def filter_access_levels(self, request):
 
-            edu_dict[_['education_level']] += _["distinct"]
+        data = request.POST
 
+        if data.get("get_csv", False):
+            s = render_to_csv_response(self.__class__.global_qs)
+            return s
+
+        curr_level = int(data.get("level", 1))
+        value = data.get("value", None)
+
+        filtered_qs = Education.objects.filter(**{"lvl{}_id".format(curr_level): value})
+        
+        self.__class__.global_qs = filtered_qs
+
+        grad_dict = self.grad_dict_generator(filtered_qs)
+        profession_dict = self.profession_dict_generator(filtered_qs)
+        edu_dict = self.edu_dict_generator(filtered_qs)
+        access_levels =  self.access_levels_generator(filtered_qs)
+
+        return JsonResponse({"access_levels": access_levels, "currLevel": curr_level, "educationLevel": edu_dict, "gradDict": grad_dict, "professionDict": profession_dict})
+
+    def access_levels_generator(self, filtered_qs):
+        access_levels = {}
         for i in range(1, 10):
-
-            level_options = queryset.values('level{}'.format(i), 'lvl{}_id'.format(i)).distinct()
+            level_options = filtered_qs.values("level{}".format(i), "lvl{}_id".format(i)).distinct()
 
             level_options = [{
                 "id": k['lvl{}_id'.format(i)],
                 "string": k['level{}'.format(i)],
-            } for k in level_options]
+            } for k in level_options if k['level{}'.format(i)] != None]
 
             access_levels[i] = level_options
-
-        return render(request, "home.html", {"access_levels": access_levels, "educationLevel": edu_dict, "gradDict": grad_dict, "professionDict": profession_dict})
+        return access_levels
 
     def profession_dict_generator(self, queryset):
         profession_dict = {
@@ -113,53 +127,7 @@ class HomeViewSet(ViewSet):
             grad_dict[str(record["anticipated_graduation_year"])] += record["users"]
         return grad_dict
 
-    def filter_access_levels(self, request):
-
-        data = request.POST
-
-        if data.get("get_csv", False):
-            s = render_to_csv_response(self.__class__.global_qs)
-            return s
-
-        curr_level = int(data.get("level", 1))
-        value = data.get("value", None)
-
-        labels = [i['education_level'] for i in Education.objects.values('education_level').distinct()]
-
-        if value == "all":
-
-            filtered_qs = Education.objects.all()
-        
-        else:
-
-            filtered_qs = Education.objects.filter(**{"lvl{}_id".format(curr_level): value})
-        
-        self.__class__.global_qs = filtered_qs
-
-        grad_dict = self.grad_dict_generator(filtered_qs)
-        profession_dict = self.profession_dict_generator(filtered_qs)
-
-        if value != None:
-
-            access_levels = {}
-
-            edu_dict = {course: 0 for course in labels}
-
-            for i in range(1, 10):
-
-                level_options = filtered_qs.values("level{}".format(i), "lvl{}_id".format(i)).distinct()
-
-                user_count = filtered_qs.values("education_level").annotate(distinct=Count("user_id"))
-
-                for _ in user_count:
-
-                    edu_dict[_['education_level']] += _["distinct"]
-
-                level_options = [{
-                    "id": k['lvl{}_id'.format(i)],
-                    "string": k['level{}'.format(i)],
-                } for k in level_options if k['level{}'.format(i)] != None]
-
-                access_levels[i] = level_options
-
-            return JsonResponse({"access_levels": access_levels, "currLevel": curr_level, "educationLevel": edu_dict, "gradDict": grad_dict, "professionDict": profession_dict})
+    def edu_dict_generator(self, filtered_qs):
+        edu_dict_values = filtered_qs.values("education_level").distinct().annotate(users=Count("user_id"))
+        edu_dict = {record["education_level"]: record["users"] for record in edu_dict_values}
+        return edu_dict
